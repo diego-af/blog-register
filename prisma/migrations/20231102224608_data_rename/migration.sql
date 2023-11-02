2b68416ddfddd34a32f68961c42382b879bafba7
Diego Alves <dyegoo.1295@gmail.com>
2023-11-02 19:47:24 -0300

    renmae datavase

--- prisma/migrations/20231102224608_data_rename/migration.sql
+++ prisma/migrations/20231102224608_data_rename/migration.sql
@@ -1,19 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropTable
-DROP TABLE "Post";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
@@ -33,9 +17,13 @@ CREATE TABLE "services" (
     "name_service" TEXT NOT NULL,
     "date" TEXT NOT NULL,
     "hours" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "services_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "services" ADD CONSTRAINT "services_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
