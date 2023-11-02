d7be4f74800b33d81416098ba4b1d81bbb9db163
Diego Alves <dyegoo.1295@gmail.com>
2023-11-02 19:42:43 -0300

    Remove bbutton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 import {Button} from '@/components/ui/button';
-import {createData} from '@/utils/CreateItem';
 
 export default function Home() {
 	return (
 		<div className='flex'>
-			<Button onClick={createData}>Button</Button>
+			<Button>Button</Button>
 		</div>
 	);
 }
